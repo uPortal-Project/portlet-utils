8957ec3125e78afd42398a480306260dcbd5d545
Eric Dalquist <eric.dalquist@gmail.com>
2012-06-22 00:07:16 -0500

    Flush out javadocs

--- spring-webmvc-portlet-contrib/src/main/java/org/jasig/springframework/web/portlet/context/ContribDispatcherPortlet.java
+++ spring-webmvc-portlet-contrib/src/main/java/org/jasig/springframework/web/portlet/context/ContribDispatcherPortlet.java
@@ -14,12 +14,28 @@ import org.springframework.web.portlet.DispatcherPortlet;
  */
 public class ContribDispatcherPortlet extends DispatcherPortlet {
 
+    /**
+     * Uses {@link PortletApplicationContextUtils2#getPortletApplicationContext(PortletContext)} to see if
+     * the portlet application level context has been loaded. If not it is loaded by the {@link PortletContextLoader}
+     * retrieved from the {@link PortletContext}. The portlet applications's context is then used as the parent for the
+     * portlet's context.
+     * <p>
+     * The check/load of the portlet app context is done within a synchronized block on the {@link PortletContextLoader}.
+     * <p>
+     * If no {@link PortletContextLoader} exists in the {@link PortletContext} (the web.xml listener was not configured)
+     * the portlet app level context loading is skipped.
+     */
     @Override
     protected ApplicationContext createPortletApplicationContext(ApplicationContext parent) {
         //Right here parent is actually the root servlet application context, we will ignore it
         
         final PortletContext portletContext = this.getPortletContext();
         final PortletContextLoader portletContextLoader = (PortletContextLoader)portletContext.getAttribute(PortletApplicationContextUtils2.ROOT_PORTLET_APPLICATION_CONTEXT_LOADER_ATTRIBUTE);
+        if (portletContextLoader == null) {
+            logger.info("No PortletContextLoader found, skipping load of portlet-app level context. See org.jasig.springframework.web.portlet.context.PortletContextLoaderListener for more information");
+            return super.createPortletApplicationContext(parent);
+        }
+        
         
         WebApplicationContext parentPortletApplicationContext;
         synchronized (portletContextLoader) {
